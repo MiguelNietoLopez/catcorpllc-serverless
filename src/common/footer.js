8ea2557052fb969d9b40640c6143f914de14349a
Miguel Nieto Lopez <miguelnietolopez2002@gmail.com>
2023-07-25 21:42:41 -0500

    t

---
+++ src/common/footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Footer() {
+    return (
+        <footer id="footer">
+            <div id="footerCopyright">Copyright 2022, Miguel Nieto Lopez</div>
+        </footer>
+    )
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./common/header";
+import Footer from "./common/footer";
+class IndexApp extends React.Component {
+    constructor(props)
+    {
+        super(props);{
 
+        }
+    }
+    render(){
+        return(
+            <main id="indexApp">
+                {Header()}
+                <div id="centerContent">
+                    <h1 id="title">Cat Corp LLC</h1>
+                    <h2 id="subtitle">Number One Cat based Software Firm</h2>
+                    <img id="img1" className="img" alt="Cat working on Computer"  src="./common/images/business-cat-in-office.jpg"></img>
+                    <p className="centerText imgDesc" id="main">We provide many different services and can help you.</p>
+                    <p className="centerText imgTitle" id="stats">Our customers see an average of:</p>
+                    <img id="img2" className="img" alt="Graph. The Information of the Graph is described as"  src="/common/images/13122-01-data-driven-infographic-16x9-1.jpg"></img>
+                    <p className="centerText imgDesc" id="stats2">5000x more purchases, 90000x more return customers, and an average life expectancy of 35000 years</p>
+                    <p className="centerText imgTitle" id="thumbsuptext">Contact Us Today for a Free quote</p>
+                    <img id="img4" className="img" src="/common/images/srdffd.PNG" alt="Happy Customers Giving A Thumbs Up"></img>
+                </div>
+                {Footer()}
+            </main>
+        )
+    }
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<IndexApp />);
